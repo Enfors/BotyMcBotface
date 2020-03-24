b1083ddd7ead4c855c39921a24060c9d762bfa7a
Christer Enfors <christer.enfors@gmail.com>
2020-03-24 19:12:43 +0100

    Set version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BotyMcBotface",
-    version="1.0.0",
+    version="1.1.0",
     author="Christer Enfors",
     author_email="christer.enfors@gmail.com",
     url="https://github.com/enfors/BotyMcBotface",
